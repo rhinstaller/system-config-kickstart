0275ad550e93746869667bce09e7c7206e3e76dd
bfox <bfox>
2001-06-22 22:34:55 +0000

    new window for adding partitions

---
+++ src/partWindow.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+## userWindow - event handling code for userconf's user window
+## Copyright (C) 2001 Red Hat, Inc.
+## Copyright (C) 2001 Brent Fox <bfox@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+## Author: Brent Fox
+
+from gtk import *
+from gnome.ui import *
+import GtkExtra
+import string
+import savedialog
+
+import gtk
+import signal
+import libglade
+import gnome.ui
+
+xml = libglade.GladeXML ("./ksconfig.glade", "partition_dialog", domain="ksconfig")
+
+class partWindow:
+
+    def destroy(self, args):
+        self.dialog.destroy()
+
+    def __init__(self):
+        print "init"
+        self.partitionDialog = xml.get_widget("partition_dialog")
+        self.partitionDialog.connect ("destroy", self.destroy)
+        self.deviceCombo = xml.get_widget("deviceCombo")
+
+        self.deviceCombo.set_text("foodog")
+
+        self.partitionDialog.show_all()
+
+#    def showWin (self):
+#        print "showing win"
+#        self.partitionDialog.show_all()
+
+
+
