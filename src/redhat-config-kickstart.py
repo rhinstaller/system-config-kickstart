6ed40444d4187812f0d160253706c0c7641713c1
bfox <bfox>
2002-07-10 21:24:30 +0000

    renamed from ksconfig

---
+++ src/redhat-config-kickstart.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2.2
+
+## Kickstart Configurator - A graphical kickstart file generator
+## Copyright (C) 2000, 2001, 2002 Red Hat, Inc.
+## Copyright (C) 2000, 2001, 2002  Brent Fox <bfox@redhat.com>
+##                                 Tammy Fox <tfox@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+## Authors: Brent Fox <bfox@redhat.com>
+##          Tammy Fox <tfox@redhat.com>
+
+import sys
+import signal
+import gtk
+import gtk.glade
+import kickstartGui
+
+try:
+    from gtk import _disable_gdk_threading
+    _disable_gdk_threading()
+except ImportError:
+    pass
+
+if __name__ == "__main__":
+    signal.signal (signal.SIGINT, signal.SIG_DFL)
+
+#xml = gtk.glade.XML ("/usr/share/redhat-config-kickstart/redhat-config-kickstart-gtk2.glade", domain="redhat-config-kickstart")
+xml = gtk.glade.XML ("redhat-config-kickstart-gtk2.glade", domain="redhat-config-kickstart")
+
+kickstartGui.kickstartGui(xml)
+
