45fbbb4a09d0999f75690cca5252d9ba7dc5eeef
bfox <bfox>
2002-11-11 15:10:48 +0000

    initial commit

---
+++ src/kickstartData.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class KickstartData:
+    def __init__(self):
+        self.lang = None
+
+
+    def setLang(self, lang):
+        self.lang = lang
+
+    def getLang(self):
+        return self.lang
+        
