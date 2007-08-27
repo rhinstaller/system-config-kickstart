5781ab96d0058d52e50a30ecd2bb087ea02329a1
clumens <clumens>
2007-08-27 20:05:13 +0000

    Remove dependency on system-config-securitylevel.

--- src/firewall.py
+++ src/firewall.py
@@ -1,5 +1,5 @@
 ## Kickstart Configurator - A graphical kickstart file generator
-## Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006 Red Hat, Inc.
+## Copyright (C) 2000, 2001, 2002, 2003 Red Hat, Inc.
 ## Copyright (C) 2000, 2001, 2002, 2003 Brent Fox <bfox@redhat.com>
 ##                                      Tammy Fox <tfox@redhat.com>
 
@@ -19,11 +19,10 @@
 
 import gtk
 import gtk.glade
-import sys
-sys.path.append("/usr/share/system-config-securitylevel")
-from securitylevel import xml as selinuxXml
-from securitylevel import childWindow as scslWindow
-from selinuxPage import selinuxPage
+import gobject
+import string
+import os
+import getopt
 from pykickstart.constants import *
 
 ##
@@ -35,108 +34,155 @@ domain = 'system-config-kickstart'
 translate.textdomain (domain)
 gtk.glade.bindtextdomain(domain)
 
-class selinuxWindow(selinuxPage):
-    def __init__(self, xml, doDebug=None, inFirstboot=False):
-        selinuxPage.__init__(self, xml, doDebug, inFirstboot)
-
-    def read_selinux_config(self):
-        return 0
-
-    def enabled_changed(self, combo):
-        pass
-
-    def apply(self, *args):
-        self.ks = args[0]
-
-        if self.enabledOptionMenu.get_active() == 0:
-            self.ks.selinux(selinux=SELINUX_ENFORCING)
-        elif self.enabledOptionMenu.get_active() == 1:
-            self.ks.selinux(selinux=SELINUX_PERMISSIVE)
-        elif self.enabledOptionMenu.get_active() == 2:
-            self.ks.selinux(selinux=SELINUX_DISABLED)
-
-class FirewallWindow(scslWindow):
-    def __init__(self):
-        scslWindow.__init__(self)
-
-    def apply(self, *args):
-        self.ks = args[0]
-        self.ks.firewall(trusts=[], ports=[],
-                         enabled=self.securityOptionMenu.get_active() == 0)
-
-        iter = self.incomingList.store.get_iter_first()
-        while iter:
-            svc = self._getServiceByDesc(self.incomingList.store.get_value(iter, 1))
-            if self.incomingList.store.get_value(iter, 0):
-                for (port, proto) in svc.ports:
-                    self.ks.firewall.ports.append("%s:%s" % (port, proto))
-
-            iter = self.incomingList.store.iter_next(iter)
-
-        model = self.otherPortsView.get_model()
-        iter = model.get_iter_first()
-
-        while iter:
-            self.ks.firewall.ports.append("%s:%s" % (model.get_value(iter, 0),
-                                                     model.get_value(iter, 1)))
-            iter = model.iter_next(iter)
+trustedStore = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING)
 
 class Firewall:
     def __init__(self, xml, ksHandler):
-        self.toplevel = xml.get_widget("main_window")
-        self.firewall_frame = xml.get_widget("firewall_frame")
         self.ks = ksHandler
+        self.firewall_frame = xml.get_widget("firewall_frame")
+        self.firewall_vbox = xml.get_widget("firewall_vbox")
+        self.firewall_label_box = xml.get_widget("firewall_label_box")
+        self.securityOptionMenu = xml.get_widget("securityOptionMenu")
+        self.selinuxOptionMenu = xml.get_widget("selinuxOptionMenu")
+        self.firewallDefaultRadio = xml.get_widget("firewallDefaultRadio")
+        self.trusted_devices_label = xml.get_widget("trusted_devices_label")
+        self.allow_incoming_label = xml.get_widget("allow_incoming_label")
+        self.fnnirewall_ports_label = xml.get_widget("firewall_ports_label")
+        self.firewall_ports_entry = xml.get_widget("firewall_ports_entry")
+        self.customTable = xml.get_widget("customTable")
+        self.customFrame = xml.get_widget("customFrame")
 
-        if self.ks.upgrade.upgrade:
-            disabledLabel = gtk.Label(_("Firewall configuration is not applicable on upgrades."))
-            disabledLabel.set_line_wrap(True)
-            self.firewall_frame.add(disabledLabel)
-            self.firewall_frame.show_all()
-            return
+        self.securityOptionMenu.connect("changed", self.disable_firewall)
 
-        # Bring in windows from system-config-securitylevel.
-        self.scsl_window = FirewallWindow()
-        self.selinuxWindow = selinuxWindow(selinuxXml)
-        self.scsl_window.setupScreen()
-        self.scsl_notebook = self.scsl_window.xml.get_widget("scsNotebook")
-        self.scsl_notebook.remove_page(1)
+        self.label2 = gtk.Label (_("Trusted services:"))
+        self.label2.set_alignment (0.0, 0.0)
 
-        # Set some defaults that can be overridden by applyKsdata later.
-        self.scsl_window.securityOptionMenu.set_active(0)
-        self.scsl_window.trustedServicesBox.set_sensitive(True)
-        self.scsl_window.otherPortsExpander.set_sensitive(True)
+        self.incomingStore = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING)
+        self.incomingView = gtk.TreeView()
+        self.incomingView.set_headers_visible(False)
+        self.incomingView.set_model(self.incomingStore)
+        checkbox = gtk.CellRendererToggle()
+        checkbox.connect("toggled", self.item_toggled, self.incomingStore)
+        col = gtk.TreeViewColumn('', checkbox, active = 0)
+        col.set_fixed_width(20)
+        col.set_clickable(True)
+        self.incomingView.append_column(col)
 
-        self.scsl_notebook.reparent(self.firewall_frame)
-        self.scsl_notebook.show_all()
+        col = gtk.TreeViewColumn("", gtk.CellRendererText(), text=1)
+        self.incomingView.append_column(col)
+
+        self.list = {"SSH":"ssh", "Telnet":"telnet", "WWW (HTTP)":"http",
+                     "Mail (SMTP)":"smtp", "FTP":"ftp"}
+
+        for item in self.list.keys():
+            iter = self.incomingStore.append()
+            self.incomingStore.set_value(iter, 0, False)
+            self.incomingStore.set_value(iter, 1, item)
+
+        viewport = gtk.Viewport()
+        viewport.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        viewport.add(self.incomingView)
+
+        self.customTable.attach (self.label2, 0, 1, 2, 3, gtk.FILL, gtk.FILL, 5, 5)
+        self.customTable.attach (viewport, 1, 2, 2, 3, gtk.EXPAND|gtk.FILL, gtk.FILL, 5, 5)
+
+        self.label3 = gtk.Label (_("Other ports: (1029:tcp)"))
+        self.label3.set_alignment (0.0, 0.0)
+        self.portsEntry = gtk.Entry ()
+        self.customTable.attach (self.label3, 0, 1, 3, 4, gtk.FILL, gtk.FILL, 5, 5)
+        self.customTable.attach (self.portsEntry, 1, 2, 3, 4, gtk.EXPAND|gtk.FILL, gtk.FILL, 5, 5)
+
+        self.firewall_vbox.show_all()
+
+    def item_toggled(self, data, row, store):
+        iter = store.get_iter((int(row),))
+        val = store.get_value(iter, 0)
+        store.set_value(iter, 0 , not val)
+
+    def disable_firewall (self, widget):
+        state = self.securityOptionMenu.get_history()
+
+        if state == 0:
+            self.customTable.set_sensitive (True)
+        else:
+            self.customTable.set_sensitive (False)
+
+    def toggle_row (self, list, row):
+        (val, row_data, header) = list.get_row_data(row)
+        val = not val
+        list.set_row_data(row, (val, row_data, header))
+        list._update_row (row)
+
+    def setSensitive(self, boolean):
+        if boolean == False:
+            self.firewall_vbox.hide()
+            self.firewall_label_box.show()
+        else:
+            self.firewall_vbox.show()
+            self.firewall_label_box.hide()
 
     def formToKickstart(self):
-        if self.ks.upgrade.upgrade:
+        if self.ks.upgrade.upgrade == True:
             return
 
-        self.scsl_window.apply(self.ks)
-        self.selinuxWindow.apply(self.ks)
+        self.ks.firewall(trusts=[], ports=[],
+                         enabled=self.securityOptionMenu.get_history() == 0)
+
+        iter = self.incomingStore.get_iter_first()
+
+        while iter:
+            if self.incomingStore.get_value(iter, 0) == True:
+                service = self.list[self.incomingStore.get_value(iter, 1)]
+                self.ks.firewall.ports.append(service)
+            iter = self.incomingStore.iter_next(iter)
+
+        self.ks.firewall.ports.extend(string.split(self.portsEntry.get_text()))
+
+        if self.selinuxOptionMenu.get_history() == 0:
+            self.ks.selinux(selinux=SELINUX_ENFORCING)
+        elif self.selinuxOptionMenu.get_history() == 1:
+            self.ks.selinux(selinux=SELINUX_PERMISSIVE)
+        elif self.selinuxOptionMenu.get_history() == 2:
+            self.ks.selinux(selinux=SELINUX_DISABLED)
 
     def applyKickstart(self):
-        # If we convert everything into a list of arguments, then
-        # system-config-securitylevel already knows how to handle it.
-        args = []
-
-        if self.ks.firewall.enabled:
-            args.append("--enabled")
+        if self.ks.firewall.enabled == True:
+            self.securityOptionMenu.set_history(0)
         else:
-            args.append("--disabled")
+            self.securityOptionMenu.set_history(1)
 
-        for port in self.ks.firewall.ports:
-            args.append("--port=%s" % port)
+        iter = self.incomingStore.get_iter_first()
 
-        if not self.ks.selinux.selinux:
-            self.ks.selinux.selinux = SELINUX_ENFORCING
+        filteredPorts = self.ks.firewall.ports
+
+        while iter:
+            service = self.list[self.incomingStore.get_value(iter, 1)]
+
+            if service == "ssh" and "22:tcp" in filteredPorts:
+                self.incomingStore.set_value(iter, 0, True)
+                filteredPorts.remove("22:tcp")
+            elif service == "telnet" and "23:tcp" in filteredPorts:
+                self.incomingStore.set_value(iter, 0, True)
+                filteredPorts.remove("23:tcp")
+            elif service == "http" and "80:tcp" in filteredPorts:
+                self.incomingStore.set_value(iter, 0, True)
+                filteredPorts.remove("80:tcp")
+                filteredPorts.remove("443:tcp")
+            elif service == "smtp" and "25:tcp" in filteredPorts:
+                self.incomingStore.set_value(iter, 0, True)
+                filteredPorts.remove("25:tcp")
+            elif service == "ftp" and "21:tcp" in filteredPorts:
+                self.incomingStore.set_value(iter, 0, True)
+                filteredPorts.remove("21:tcp")
+
+            iter = self.incomingStore.iter_next(iter)
+
+        if len(self.ks.firewall.ports) > 0:
+            self.portsEntry.set_text(string.join(filteredPorts, ","))
 
         if self.ks.selinux.selinux == SELINUX_DISABLED:
-            self.selinuxWindow.enabledOptionMenu.set_active(2)
+            self.selinuxOptionMenu.set_history(2)
         elif self.ks.selinux.selinux == SELINUX_ENFORCING:
-            self.selinuxWindow.enabledOptionMenu.set_active(0)
+            self.selinuxOptionMenu.set_history(0)
         elif self.ks.selinux.selinux == SELINUX_PERMISSIVE:
-            self.selinuxWindow.enabledOptionMenu.set_active(1)
-
-        self.scsl_window.parseArgList(args)
+            self.selinuxOptionMenu.set_history(1)
