521a1019eb44a26e50dc4b1cf557bb3f389a1f44
clumens <clumens>
2007-08-27 20:37:20 +0000

    Update licensing information.

--- src/install.py
+++ src/install.py
@@ -1,23 +1,24 @@
-## Kickstart Configurator - A graphical kickstart file generator
-## Copyright (C) 2000, 2001, 2002, 2003 Red Hat, Inc.
-## Copyright (C) 2000, 2001, 2002, 2003 Brent Fox <bfox@redhat.com>
-##                                      Tammy Fox <tfox@redhat.com>
-
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-#Kickstart Configurator Installation Methods
+#
+# Chris Lumens <clumens@redhat.com>
+# Brent Fox <bfox@redhat.com>
+# Tammy Fox <tfox@redhat.com>
+#
+# Copyright (C) 2000-2007 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2 or, at your option, any later version.  This
+# program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  Any Red Hat
+# trademarks that are incorporated in the source code or documentation are not
+# subject to the GNU General Public License and may only be used or replicated
+# with the express permission of Red Hat, Inc. 
 
 import gtk
 import gtk.glade
